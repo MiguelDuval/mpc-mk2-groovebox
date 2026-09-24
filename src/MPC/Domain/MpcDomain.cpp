df9db924df721049ce6d9f8d0d0945ae70a25e3e
MiguelDuval <88213910+MiguelDuval@users.noreply.github.com>
2026-09-24 18:13:40 +0200

    feat: establish MPC domain model

---
+++ src/MPC/Domain/MpcDomain.cpp
@@ -0,0 +1,10 @@
+#include "MpcDomain.h"
+
+#include <utility>
+
+namespace mpc::domain {
+
+static_assert(kMaxProgramPads == 128);
+static_assert(kMaxSampleLayers == 8);
+
+} // namespace mpc::domain
