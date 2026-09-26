1a2e4f5f1cacc1edd3381d873c7fa7afa05a671a
MiguelDuval <88213910+MiguelDuval@users.noreply.github.com>
2026-09-26 12:17:11 +0200

    refactor: use shared sampler layer grid type

--- src/Audio/AudioEngine.cpp
+++ src/Audio/AudioEngine.cpp
@@ -27,8 +27,7 @@ namespace mpc::audio {
 class AudioEngine::OutputCallback final : public oboe::AudioStreamDataCallback {
 public:
     OutputCallback(
-            std::array<std::array<std::shared_ptr<const SampleBuffer>, kSampleLayerCount>, kPadCount>
-                    samples,
+            AudioEngine::SampleLayerGrid samples,
             std::array<std::atomic<std::uint32_t>, kPadCount>& triggerSequence,
             std::array<std::atomic<std::uint32_t>, kPadCount>& triggerVelocity,
             std::array<std::atomic<std::int32_t>, kPadCount>& tuningMilliSemitones,
@@ -234,7 +233,7 @@ private:
         bool active = false;
     };
 
-    std::array<std::array<std::shared_ptr<const SampleBuffer>, kSampleLayerCount>, kPadCount> samples_;
+    AudioEngine::SampleLayerGrid samples_;
     std::array<std::atomic<std::uint32_t>, kPadCount>& triggerSequence_;
     std::array<std::atomic<std::uint32_t>, kPadCount>& triggerVelocity_;
     std::array<std::atomic<std::int32_t>, kPadCount>& tuningMilliSemitones_;
@@ -428,8 +427,7 @@ std::string AudioEngine::start() {
         return status();
     }
 
-    std::array<std::array<std::shared_ptr<const SampleBuffer>, kSampleLayerCount>, kPadCount>
-            samples{};
+    AudioEngine::SampleLayerGrid samples{};
     bool anySample = false;
 
     for (std::size_t pad = 0; pad < kPadCount; ++pad) {
